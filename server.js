6ce490ee684be2d2dda03279d76f8d344826fb70
Anthony <perezant121@gmail.com>
2022-09-21 17:27:42 -0400

    setup functions for each option in prompt

--- server.js
+++ server.js
@@ -1,13 +1,50 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 require('console.table');
-
 const prompt = inquirer.createPromptModule();
+
+//establishes connection to mysql database
 const db = mysql.createConnection({
     user: 'root',
     database: 'employee_db',
 });
 
+//Displays all the departments in database
+const viewAllDepartments = () => {
+    
+};
+
+//Displays all the roles in database
+const viewAllRoles = () => {
+    
+};
+
+//Displays all the employees in database
+const viewAllEmployees = () => {
+    
+};
+
+//Adds a deapartment to the database
+const addDapartment = () => {
+    
+}
+
+//Adds a role to the database
+const addRole = () => {
+    
+}
+
+//Adds an employee to the database
+const addEmployee = () => {
+    
+}
+
+//Updates an employee in the database
+const updateEmployeeRole = () => {
+    
+}
+
+//Initial promt with a switch case to run individual functions. 
 const init = () => {
     prompt({
         type: 'rawlist',
@@ -44,7 +81,7 @@ const init = () => {
             return addEmployee();
             }
             case 'Update an Employee Role': {
-            return updateEmployee();
+            return updateEmployeeRole();
             }
             default: {
                 return process.exit();
